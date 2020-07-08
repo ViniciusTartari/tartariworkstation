c9a818719fa718a06391f81a1f095bdaaaccad3f
Vinicius Tartari <vinitartari@gmail.com>
2020-07-08 17:46:28 +0200

    Updated zshScript.sh

--- zshScript.sh
+++ zshScript.sh
@@ -3,10 +3,13 @@
 ## zsh - Install and configuration script
 ## by: Vinicius Tartari - tested in Debian 10 (buster)
 
+# Store name of current directory
+DIRECTORY=$(cd `dirname $0` && pwd)
+
 echo -e "\033[01;32mzsh - Install and configuration script - by Vinicius Tartari\033[0m"
 # --------------------------------------------
 echo -e "\033[01;33mInstall zsh\033[0m"
-sudo apt install zsh
+sudo apt install zsh -y
 
 echo -e "\033[01;33mOh My Zsh - preconfiguration to Zsh \033[0m"
 sudo curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
@@ -24,15 +27,18 @@ echo -e "\033[01;33mzsh-autosuggestions\033[0m"
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 echo -e "\033[01;33mfzf\033[0m"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install -y
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
 # --------------------------------------------
 echo -e "\033[01;32mAdding visual resources\033[0m"
 
 echo -e "\033[01;33mDownloading and installing font\033[0m"
-cd Downloads
+cd ~/Downloads
 wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v1.2.0/Hack.zip
-mkdir ~/.fonts && cd ~/.fonts
+if [ ! -d ~/.fonts ]; then
+  mkdir ~/.fonts
+fi
+cd ~/.fonts
 unzip ~/Downloads/Hack.zip
 rm ~/Downloads/Hack.zip
 
@@ -41,11 +47,12 @@ gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type=b
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Hack Nerd Font Mono Regular 11"
 
 echo -e "\033[01;33mAdding Power Level 10K Theme\033[0m"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 # --------------------------------------------
 echo -e "\033[01;32mUpdating config file (.zshrc)\033[0m"
 # Removing possible .zshrc file
 rm ~/.zshrc
+cd $DIRECTORY
 cp zshrc-configfile.txt ~/.zshrc
 
 echo -e "\033[01;32mZSH configured. Restart to start to use!\033[0m"
